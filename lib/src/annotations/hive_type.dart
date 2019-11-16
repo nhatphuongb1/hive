a2c4bb1ccd828119a24e09dde97c0eec458acf55
Razvan Cristian Lung <lung.razvan@gmail.com>
2019-11-16 10:39:14 +0200

    add typeId to HiveType

--- lib/src/annotations/hive_type.dart
+++ lib/src/annotations/hive_type.dart
@@ -5,6 +5,10 @@ class HiveType {
   /// The name of the generated adapter.
   final String adapterName;
 
+  /// Identifies the type. It must be unique and must not change. If
+  /// absent the generator will not register it with Hive.
+  final int typeId;
+
   /// If [adapterName] is not set, it'll be `"YourClass" + "Adapter"`.
-  const HiveType({this.adapterName});
+  const HiveType({this.adapterName, this.typeId});
 }
